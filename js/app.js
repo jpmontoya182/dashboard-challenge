95fa3e3896eb596682fe0fc5e36b260d3ac68dab
Juan P. Montoya <Juan.Montoya@viamericas.com>
2020-05-27 10:27:35 -0500

    it was implemented logic in dark mode when the user want to change(not automactly)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 const checkbox = document.querySelector("#checkbox");
-
-console.log(window.matchMedia("(prefers-color-scheme: dark))"));
-
 // here we detect mode dark
 if (window.matchMedia("(prefers-color-scheme: dark))").matches) {
-  console.log("x");
   checkbox.setAttribute("checked", true);
 }
+
+// here register and un-register classes
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("is-dark-mode");
+    document.body.classList.remove("is-light-mode");
+  } else {
+    document.body.classList.add("is-light-mode");
+    document.body.classList.remove("is-dark-mode");
+  }
+});
